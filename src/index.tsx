48f56e1aeb02d22bd806ac12be5aa6bab29503c6
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-12-25 15:24:06 -0300

    refactor: update styles and add components on index

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import './styles.css';
 import Header from './components/header';
+import Content from './components/content';
+import Footer from './components/footer';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <Content />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
