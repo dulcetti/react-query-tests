3ee4508c072519b281fb126931791f13f6940bbf
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-12-26 21:14:42 -0300

    feat: add button and method on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,15 @@
 import logo from '../../assets/images/logo192.png';
 import './styles.css';
+import Button from '../button';
+import { GetAddress } from '../../services/cep/index';
 
 export default function Header() {
+  const contentMethod = () => GetAddress('20541145');
+
   return (
     <header className='header'>
       <img src={logo} className='logo' alt='logo' />
+      <Button label='Update content state' method={contentMethod} />
     </header>
   );
 }
